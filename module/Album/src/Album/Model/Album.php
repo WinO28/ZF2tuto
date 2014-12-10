8bc10894413ec83bc192dd1cd38fdff5df23548b
WinO <hugo.caplin@gmail.com>
2014-12-10 16:29:34 +0100

    Ajout du Formulaire d'édition

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -18,6 +18,12 @@ class Album
 		$this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
 		$this->title  = (!empty($data['title'])) ? $data['title'] : null;
 	}
+	
+	public function getArrayCopy()
+	{
+		return get_object_vars($this);
+	}
+	
 	public function setInputFilter(InputFilterInterface $inputFilter)
 	{
 		throw new \Exception("Not used");
